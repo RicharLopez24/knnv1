fc4007087038d31125026a56495ac8492c16ad42
ange_ricardo_lopez_jimenez <richar.lopez2403@gmail.com>
2021-08-24 13:51:10 -0500

    clasificacion con puntos

---
+++ JavaApplication23/src/javaapplication23/JavaApplication23.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication23;
+
+import JavaApplication2.K_means;
+
+/**
+ *
+ * @author carol
+ */
+public class JavaApplication23 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        K_means kmeans = new K_means();
+        kmeans.setVisible(true);
+    }
+    
+}
